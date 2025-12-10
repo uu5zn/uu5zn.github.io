2e6f693dd4bcd042f9aab388db11d59e1d7eb6c6
uu5zn <49563152@qq.com>
2025-12-10 09:31:54 +0800

    de

--- src/import akshare as ak.py
+++
@@ -1,8 +0,0 @@
-import akshare as ak
-import pandas as pd
-data = ak.macro_china_shibor_all()
-print(data.head())
-data['日期'] = pd.to_datetime(data['日期'], errors='coerce', format='%Y-%m-%d')
-data = data.set_index('日期')
-series_data = data['1M-定价'].iloc[-300:]
-print(series_data.head())
